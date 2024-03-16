d22e2d289e864d58ecfa14ec5ee39bd105c3542a
Viruzaum <gustavomelati@gmail.com>
2024-03-16 11:08:24 -0300

    add nix lsp to helix

--- user/shell/helix.nix
+++ user/shell/helix.nix
@@ -1,6 +1,23 @@
-{...}: {
+{pkgs, ...}: {
   programs.helix = {
     enable = true;
     defaultEditor = true;
+    languages = {
+      language-server = {
+        nil.command = "${pkgs.nil}/bin/nil";
+      };
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+          language-servers = [{name = "nil";}];
+          formatter.command = "${pkgs.alejandra}/bin/alejandra";
+        }
+        {
+          name = "fish";
+          auto-format = true;
+        }
+      ];
+    };
   };
 }

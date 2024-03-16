5d9f33217bda34998f7e59d490637e2bf89ef3e6
Viruzaum <gustavomelati@gmail.com>
2024-03-16 13:04:15 -0300

    fix gamescope/mangohud on steam

--- system/app/steam.nix
+++ system/app/steam.nix
@@ -1,6 +1,26 @@
-{...}: {
+{pkgs, ...}: {
   hardware.opengl.driSupport32Bit = true;
-  programs.steam.enable = true;
+
+  programs.steam = {
+    enable = true;
+
+    # fix gamescope inside steam
+    package = pkgs.steam.override {
+      extraPkgs = pkgs:
+        with pkgs; [
+          keyutils
+          libkrb5
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+        ];
+    };
+  };
 
   nixpkgs.overlays = [
     (final: prev: {
@@ -9,13 +29,11 @@
           (extraLibraries pkgs')
           ++ [
             pkgs'.gperftools
-            pkgs'.mangohud
-            pkgs'.gamemode
           ];
       });
     })
   ];
 
   programs.gamemode.enable = true;
-  programs.steam.gamescopeSession.enable = true;
+  #programs.steam.gamescopeSession.enable = true;
 }

d38daa3b09a69ab027b2f6deeda1cd58b8142aa5
Viruzaum <gustavomelati@gmail.com>
2024-09-15 11:23:40 -0300

    fix source engine games crashing

--- home/programs/wayland/default.nix
+++ home/programs/wayland/default.nix
@@ -28,7 +28,7 @@
   # make stuff work on wayland
   home.sessionVariables = {
     QT_QPA_PLATFORM = "wayland";
-    SDL_VIDEODRIVER = "wayland";
+    # SDL_VIDEODRIVER = "wayland";
     XDG_SESSION_TYPE = "wayland";
   };
 }

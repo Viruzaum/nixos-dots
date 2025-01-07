b927c03c47c69328025a6fa09ecb46bd5e4b2693
Viruzaum <gustavomelati@hotmail.com>
2025-01-07 14:34:13 -0300

    fix screen not turning off

--- home/system/hypridle/default.nix
+++ home/system/hypridle/default.nix
@@ -1,6 +1,6 @@
 {
   pkgs,
-  inputs,
+  config,
   lib,
   ...
 }: {
@@ -8,19 +8,17 @@
     enable = true;
     settings = {
       general = {
-        # lock_cmd = lib.getExe inputs.hyprlock.packages.${pkgs.system}.hyprlock;
-        lock_cmd = lib.getExe pkgs.hyprlock;
+        lock_cmd = "$pidof hyprlock || {lib.getExe pkgs.hyprlock}";
         unlock_cmd = "killall -USR1 hyprlock";
         before_sleep_cmd = "${lib.getExe' pkgs.systemd "loginctl"} lock-session";
-        # after_sleep_cmd = "${lib.getExe' inputs.hyprland.packages.${pkgs.system}.hyprland "hyprctl"} dispatch dpms on";
         after_sleep_cmd = "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms on";
         ignore_dbus_inhibit = false;
       };
       listener = [
         {
           timeout = 300;
-          on-timeout = "${lib.getExe pkgs.brightnessctl} -s set 0";
-          on-resume = "${lib.getExe pkgs.brightnessctl} -r";
+          on-timeout = "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms off"; # screen off when timeout has passed
+          on-resume = "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms on"; # screen on when activity is detected after timeout has fired.        }
         }
         {
           timeout = 630;

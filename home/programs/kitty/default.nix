3549b3a03938b4e830a520d2378345f426db87d3
Viruzaum <gustavomelati@hotmail.com>
2025-08-06 23:46:57 -0300

    make everything transparent

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -3,7 +3,7 @@
     enable = true;
     themeFile = "GruvboxMaterialDarkHard";
     settings = {
-      background_opacity = lib.mkForce "0.8";
+      # background_opacity = lib.mkForce "0.8";
     };
     shellIntegration.enableFishIntegration = true;
   };
--- home/system/niri/rules.nix
+++ home/system/niri/rules.nix
@@ -11,6 +11,7 @@
       };
       clip-to-geometry = true;
       draw-border-with-background = false;
+      opacity = 0.93;
     }
     {
       matches = [

168abf59a6c3aa4f6bf9e5d03b41626c9b4d61a4
Viruzaum <gustavomelati@hotmail.com>
2025-08-06 23:48:23 -0300

    some niri bind changes

--- home/system/niri/binds.nix
+++ home/system/niri/binds.nix
@@ -20,10 +20,11 @@
     "XF86AudioRaiseVolume".action = set-volume "5%+";
     "XF86AudioLowerVolume".action = set-volume "5%-";
 
-    "Print".action = screenshot;
+    "Print".action = screenshot {show-pointer = false;};
     "Ctrl+Print".action.screenshot-screen = [];
     "Shift+Print".action = screenshot-window;
     "Mod+D".action = spawn "walker";
+    "Mod+V".action = spawn "walker" "-m" "clipboard";
     "Mod+B".action = spawn "zen";
     "Mod+Return".action = spawn "kitty";
     "Mod+R".action = switch-preset-column-width;
@@ -45,13 +46,13 @@
 
     "Mod+Comma".action = consume-window-into-column;
     "Mod+Period".action = expel-window-from-column;
-    # "Mod+C".action = center-visible-columns;
+    "Mod+C".action = center-visible-columns;
     "Mod+Tab".action = switch-focus-between-floating-and-tiling;
 
     "Mod+Minus".action = set-column-width "-10%";
-    "Mod+Plus".action = set-column-width "+10%";
+    "Mod+Equal".action = set-column-width "+10%";
     "Mod+Shift+Minus".action = set-window-height "-10%";
-    "Mod+Shift+Plus".action = set-window-height "+10%";
+    "Mod+Shift+Equal".action = set-window-height "+10%";
 
     "Mod+H".action = focus-column-or-monitor-left;
     "Mod+L".action = focus-column-or-monitor-right;
@@ -97,24 +98,24 @@
     "Mod+Ctrl+WheelScrollRight".action = move-column-right;
     "Mod+Ctrl+WheelScrollLeft".action = move-column-left;
 
-    "Mod+1".action = focus-workspace 1;
-    "Mod+2".action = focus-workspace 2;
-    "Mod+3".action = focus-workspace 3;
-    "Mod+4".action = focus-workspace 4;
-    "Mod+5".action = focus-workspace 5;
-    "Mod+6".action = focus-workspace 6;
-    "Mod+7".action = focus-workspace 7;
-    "Mod+8".action = focus-workspace 8;
-    "Mod+9".action = focus-workspace 9;
+    # "Mod+1".action = focus-workspace 1;
+    # "Mod+2".action = focus-workspace 2;
+    # "Mod+3".action = focus-workspace 3;
+    # "Mod+4".action = focus-workspace 4;
+    # "Mod+5".action = focus-workspace 5;
+    # "Mod+6".action = focus-workspace 6;
+    # "Mod+7".action = focus-workspace 7;
+    # "Mod+8".action = focus-workspace 8;
+    # "Mod+9".action = focus-workspace 9;
 
-    "Mod+Ctrl+1".action.move-window-to-workspace = 1;
-    "Mod+Ctrl+2".action.move-window-to-workspace = 2;
-    "Mod+Ctrl+3".action.move-window-to-workspace = 3;
-    "Mod+Ctrl+4".action.move-window-to-workspace = 4;
-    "Mod+Ctrl+5".action.move-window-to-workspace = 5;
-    "Mod+Ctrl+6".action.move-window-to-workspace = 6;
-    "Mod+Ctrl+7".action.move-window-to-workspace = 7;
-    "Mod+Ctrl+8".action.move-window-to-workspace = 8;
-    "Mod+Ctrl+9".action.move-window-to-workspace = 9;
+    # "Mod+Ctrl+1".action.move-window-to-workspace = 1;
+    # "Mod+Ctrl+2".action.move-window-to-workspace = 2;
+    # "Mod+Ctrl+3".action.move-window-to-workspace = 3;
+    # "Mod+Ctrl+4".action.move-window-to-workspace = 4;
+    # "Mod+Ctrl+5".action.move-window-to-workspace = 5;
+    # "Mod+Ctrl+6".action.move-window-to-workspace = 6;
+    # "Mod+Ctrl+7".action.move-window-to-workspace = 7;
+    # "Mod+Ctrl+8".action.move-window-to-workspace = 8;
+    # "Mod+Ctrl+9".action.move-window-to-workspace = 9;
   };
 }

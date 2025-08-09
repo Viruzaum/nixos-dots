fb0a8f21e762f2e6c3f029e4c901a8454b77a18b
Viruzaum <gustavomelati@hotmail.com>
2025-08-09 11:09:51 -0300

    disable some things this system doesn't need anymore

--- hosts/mimi/home.nix
+++ hosts/mimi/home.nix
@@ -10,29 +10,29 @@
     # Programs
     ../../home/programs/helix
     ../../home/programs/shell
-    ../../home/programs/nushell
+    # ../../home/programs/nushell
     ../../home/programs/fastfetch
     ../../home/programs/git
     ../../home/programs/kitty
-    ../../home/programs/mpv
+    # ../../home/programs/mpv
     ../../home/programs/mpd
     ../../home/programs/beets
     ../../home/programs/lazygit
     ../../home/programs/nh
     ../../home/programs/syncthing
-    ../../home/programs/mangohud
-    ../../home/programs/mako
-    ../../home/programs/kdeconnect
-    ../../home/programs/fcitx5
+    # ../../home/programs/mangohud
+    # ../../home/programs/mako
+    # ../../home/programs/kdeconnect
+    # ../../home/programs/fcitx5
     ../../home/programs/nix-index
-    ../../home/programs/thunderbird
-    ../../home/programs/walker
+    # ../../home/programs/thunderbird
+    # ../../home/programs/walker
 
     # Scripts
     #../../home/scripts # All scripts
 
     # System (Desktop environment like stuff)
-    ../../home/system/niri
+    # ../../home/system/niri
     # ../../home/system/quickshell
     # ../../home/system/hyprland
     # ../../home/system/hypridle
@@ -40,7 +40,7 @@
     # ../../home/system/hyprpanel
     # ../../home/system/hyprpaper
     # ../../home/system/gtk
-    ../../home/system/xdg
+    # ../../home/system/xdg
 
     inputs.agenix.homeManagerModules.age
   ];
@@ -54,39 +54,39 @@
       # (vesktop.override {
       #   withSystemVencord = false;
       # })
-      equibop
-      discord
+      # equibop
+      # discord
       bitwarden
-      inputs.zen-browser.packages.${pkgs.system}.default
-      anki-bin
-      inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin
+      # inputs.zen-browser.packages.${pkgs.system}.default
+      # anki-bin
+      # inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin
       deluge-gtk
-      intel-gpu-tools
+      # intel-gpu-tools
       nicotine-plus
-      zapzap
-      syncplay
+      # zapzap
+      # syncplay
       # inputs.umu.packages.${pkgs.system}.umu-launcher
-      lutris
-      zed-editor
-      prismlauncher
+      # lutris
+      # zed-editor
+      # prismlauncher
       statix
       manix
       deadnix
       inputs.agenix.packages.${pkgs.system}.default
-      tealdeer
+      # tealdeer
       zellij
       # nur.repos.ataraxiasjel.waydroid-script
       # cinny-desktop
-      olympus
-      telegram-desktop
-      mission-center
-      modrinth-app
+      # olympus
+      # telegram-desktop
+      # mission-center
+      # modrinth-app
       btop
-      nautilus
+      # nautilus
     ];
 
     # Import my profile picture, used by the hyprpanel dashboard
-    file.".profile_picture.png" = {source = ./profile_picture.png;};
+    # file.".profile_picture.png" = {source = ./profile_picture.png;};
 
     # Don't touch this
     stateVersion = "23.11";

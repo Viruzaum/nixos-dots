d95002eb7fadf0f12a961140f96b8d5c84c725e8
viruz <gustavomelati@hotmail.com>
2025-09-06 01:12:19 -0300

    feat: use niri fullscreen animation pr

--- nixos/niri.nix
+++ nixos/niri.nix
@@ -8,7 +8,17 @@
 
   programs.niri = {
     enable = true;
-    package = pkgs.niri-stable;
+    package = pkgs.niri.overrideAttrs (o: {
+      patches =
+        (o.patches or [])
+        ++ [
+          (pkgs.fetchpatch {
+            url = "https://patch-diff.githubusercontent.com/raw/YaLTeR/niri/pull/2333.diff";
+            hash = "sha256-MN3/GyuTKEj7GIZEN/woFzY3xD/tSSeddvvKpAt6czc=";
+            name = "fullscreen-refactor";
+          })
+        ];
+    });
   };
 
   services.displayManager.gdm = {

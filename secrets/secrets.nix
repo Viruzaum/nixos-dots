77b20e5513b02fdbd2af6480c00b9e46def6e4e4
viruz <gustavomelati@hotmail.com>
2025-09-05 23:23:23 -0300

    feat: add yun to tailscale secret thing

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,9 @@
 let
   viruz = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEoxeBCvP4D+MaU44pHL0lIv1sy65HaNAvQZ+Ok6kShX";
 
-  # mimi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7k8jW8T4hhPkVs1Z4cH4J/MdcRuM1Bsi/DnTjdy7pu";
   mimi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWRTuKTAw4d8FEPpOiYSVVlykuTMo48HArkH+73Do4j";
+  yun = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICUjzI6ksfksTSmdxg6SS+MaryygbSe7g22ltnFOGB0F viruz@yun";
 in {
-  "tailscale.age".publicKeys = [viruz mimi];
+  "tailscale.age".publicKeys = [viruz mimi yun];
   "kuroneko.age".publicKeys = [viruz mimi];
-  # "github.age".publicKeys = [viruz mimi];
-  "deluge.age".publicKeys = [viruz mimi];
 }

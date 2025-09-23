6de88b6952198eef8500e6b61f7aba3d90cca2b1
viruz <gustavomelati@hotmail.com>
2025-09-22 23:08:25 -0300

    feat: update elephant to fix it not starting

--- home/programs/walker/default.nix
+++ home/programs/walker/default.nix
@@ -1,33 +1,8 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{inputs, ...}: {
   imports = [inputs.walker.homeManagerModules.default];
 
   programs.walker = {
     enable = true;
-    # package = pkgs.walker;
     runAsService = true;
-    config = {
-      terminal = "kitty";
-      builtins = {
-        applications = {};
-        bookmarks = {
-          entries = [
-            {
-              label = "Walker";
-              url = "https://github.com/abenz1267/walker";
-              keywords = ["walker"];
-            }
-          ];
-        };
-        calc = {
-          min_chars = 3;
-        };
-        clipboard = {};
-        windows = {};
-      };
-    };
   };
 }

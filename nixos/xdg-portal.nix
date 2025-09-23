9e5de9c19f4a1ec51084ce6d93a196ddf3a49000
viruz <gustavomelati@hotmail.com>
2025-09-22 23:08:25 -0300

    feat: change portals to use gtk for file chooser

--- nixos/xdg-portal.nix
+++ nixos/xdg-portal.nix
@@ -1,12 +1,15 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
-    configPackages = [config.programs.niri.package];
+    config = {
+      niri = {
+        default = ["gtk" "gnome"];
+        "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+        "org.freedesktop.impl.portal.RemoteDesktop" = "gnome";
+        "org.freedesktop.impl.portal.FileChooser" = "gtk";
+      };
+    };
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-gnome
